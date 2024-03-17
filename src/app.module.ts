a227db0dfd510fc5b68caf82c298916ec68ae0c3
mmaksi <mmaksi.dev@gmail.com>
2024-03-17 07:19:34 +0100

    created a global interceptor to add the user to the request object and a custom decorator to extract the global user object

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CurrentUserInterceptor } from './users/interceptors/current-user.interceptor';
 
 @Module({
   imports: [
@@ -18,12 +16,6 @@ import { CurrentUserInterceptor } from './users/interceptors/current-user.interc
     }),
     UsersModule,
     ReportsModule,
-  ],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
   ],
 })
 export class AppModule {}
